7df0dc206b0be76830ce1a558e7991787269e87a
AndreaVk <vico.ac1996@gmail.com>
2022-02-02 19:28:17 +0100

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+<div class="container header">
+        <div class="logo">
+            <img src="{{asset('img/dc-logo.png')}}" alt="dc-logo">
+        </div>
+        <nav>
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+        </nav>
+    </div>
+</header>
