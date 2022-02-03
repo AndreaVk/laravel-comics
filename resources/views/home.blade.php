4a1161368581cb63735d54ec746b17cfc5f65d76
AndreaVk <vico.ac1996@gmail.com>
2022-02-03 19:08:42 +0100

    fine main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,4 +19,32 @@
         </div>
         <button class="btn">Load more</button>
 </div>
+<div class="end-main">
+<div class="icon-container">
+        <div class="box">
+        <div class="icons">
+                <img src="img/buy-comics-digital-comics.png" alt="">
+                <span>Digital comics</span>
+        </div>
+        <div class="icons">
+                <img src="img/buy-comics-merchandise.png" alt="">
+                <span>DC merchandise</span>
+        </div>
+        <div class="icons">
+                <img src="img/buy-comics-subscriptions.png" alt="">
+                <span>Subscription</span>
+        </div>
+        <div class="icons">
+                <img class="sp" src="img/buy-comics-shop-locator.png" alt="">
+                <span>Comic Shop Locator</span>
+        </div>
+        <div class="icons">
+                <img src="img/buy-dc-power-visa.svg" alt="">
+                <span>Dc power visa</span>
+        </div>
+        </div>
+        
+</div>
+</div>
+
 @endsection
