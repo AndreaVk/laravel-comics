d378dab76791a961583fc9d9e61a33eaf7139250
AndreaVk <vico.ac1996@gmail.com>
2022-02-03 09:23:40 +0100

    stampa card main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,5 +7,16 @@
 @section('pageContent')
 <div class="bg-jumbo">
 </div>
-<div class="container"></div>
+<div class="container">
+        <h2>Current Series</h2>
+        <div class="card-container">
+                @foreach($comics as $comic)
+                <div class="cm-card">
+                        <img src="{{$comic['thumb']}}" alt="">
+                        <p>{{$comic['series']}}</p>
+                </div>
+                @endforeach
+        </div>
+        <button class="btn">Load more</button>
+</div>
 @endsection
